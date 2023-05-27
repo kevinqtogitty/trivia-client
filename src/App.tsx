d7fcd24b621f984cd106a39e2ca92d42a90ba997
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-05-27 03:22:51 -0400

    Update: updated question state

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,25 @@ import Question from './types/types';
 import formatQuestion from './utils/formatQuestion';
 import DarkLightModeToggle from './components/DarkLightModeToggle';
 import Choice from './components/Choice';
+import DroppableArea from './components/DroppableArea';
+import { DndContext, DragEndEvent, Over } from '@dnd-kit/core';
 
 const baseUrl = 'http://localhost:3001/question';
 
 function App() {
-  const [question, setQuestion] = useState<Question>();
-  const [categories, setCategories] = useState<string[]>();
   const [isDarkMode, setIsDarkMode] = useState(true);
   const [isDropped, setIsDropped] = useState(false);
+  const [questionState, setQuestionState] = useState<{
+    question: Question | null;
+    isNewQuestion: boolean;
+    correctAnswerDropped: boolean;
+    choiceDropped: string | null;
+  }>({
+    question: null,
+    isNewQuestion: true,
+    correctAnswerDropped: false,
+    choiceDropped: null
+  });
 
   useEffect(() => {
     fetchRandomQuestion();
@@ -21,7 +32,41 @@ function App() {
     if (data) {
       const question = await data.json();
       const formattedQuestion = formatQuestion(question[0]);
-      setQuestion(formattedQuestion);
+      setQuestionState((state) => {
+        return { ...state, question: formattedQuestion };
+      });
+    }
+  };
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const {
+      active: {
+        data: { current }
+      },
+      over
+    } = event;
+
+    if (over) {
+      setIsDropped(true);
+      if (current?.isCorrect) {
+        setQuestionState((state) => {
+          return {
+            ...state,
+            correctAnswerDropped: true,
+            choiceDropped: current?.text
+          };
+        });
+        // border green
+      } else {
+        setQuestionState((state) => {
+          return {
+            ...state,
+            correctAnswerDropped: false,
+            choiceDropped: current?.text
+          };
+        });
+        // border red
+      }
     }
   };
 
@@ -31,15 +76,22 @@ function App() {
         isDarkMode={isDarkMode}
         setIsDarkMode={setIsDarkMode}
       />
-      <div className={`main-container ${isDarkMode ? '' : 'light-mode'}`}>
-        <h2 className="question">{question?.question.text}</h2>
-
-        <ul className="choice-container">
-          {question?.possibleAnswers.map((choice) => (
-            <Choice text={choice.text} />
-          ))}
-        </ul>
-      </div>
+      <DndContext onDragEnd={handleDragEnd}>
+        <div className={`main-container ${isDarkMode ? '' : 'light-mode'}`}>
+          <h2 className="question">{questionState.question?.question.text}</h2>
+          <DroppableArea isDropped={isDropped} questionState={questionState} />
+          <ul className="choice-container">
+            {questionState.question?.possibleAnswers?.map((choice, i) => (
+              <Choice
+                key={i}
+                text={choice.text}
+                isCorrect={choice.isCorrect}
+                choiceId={i}
+              />
+            ))}
+          </ul>
+        </div>
+      </DndContext>
     </>
   );
 }
