44c062745039a3c447fb5a6b0c652baab012d648
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-05-27 03:22:13 -0400

    Update: added dndkit drggability

--- src/components/Choice.tsx
+++ src/components/Choice.tsx
@@ -1,21 +1,45 @@
 import React from 'react';
 import { useSpring, a } from '@react-spring/web';
 import { useDrag } from '@use-gesture/react';
+import { useDraggable } from '@dnd-kit/core';
 
 interface Props {
   text: string;
+  isCorrect: boolean;
+  choiceId: number;
 }
 
-const Choice: React.FC<Props> = ({ text }) => {
-  const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }));
-
-  // Set the drag hook and define component movement based on gesture data
-  const bind = useDrag(({ down, movement: [mx, my] }) => {
-    api.start({ x: down ? mx : 0, y: down ? my : 0, immediate: down });
+const Choice: React.FC<Props> = ({ text, isCorrect, choiceId }) => {
+  // const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }));
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: choiceId,
+    data: {
+      isCorrect,
+      text
+    }
   });
 
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`
+      }
+    : undefined;
+
+  // Set the drag hook and define component movement based on gesture data
+  // const bind = useDrag(({ down, movement: [mx, my] }) => {
+  //   api.start({ x: down ? mx : 0, y: down ? my : 0, immediate: down });
+  // });
+
   return (
-    <a.li className="choice" {...bind()} style={{ x, y, touchAction: 'none' }}>
+    <a.li
+      ref={setNodeRef}
+      {...listeners}
+      {...attributes}
+      className="choice"
+      style={style}
+      // {...bind()}
+      // style={{ x, y, touchAction: 'none' }}
+    >
       {text}
     </a.li>
   );
